4ea1f25725bd6962143794964f33c6a23b9be700
ypq123456789 <114487221+ypq123456789@users.noreply.github.com>
2024-07-06 21:11:06 +0800

    Update change_root_password.sh

--- change_root_password.sh
+++ change_root_password.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "版本：0.7"
+echo "版本：0.8"
 echo "本脚本只适用于快速改root密码抢别人送的vps，不适宜用于自用机子，更不适用于生产环境，如果你在自用机子和生产环境上使用本脚本导致无法连接上ssh，后果自负！！！"
 
 # 检查是否为 root 用户
@@ -9,10 +9,34 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-# 检查是否允许root密码登录
+# 检查SSH设置并提供警告
 check_root_login() {
-    if grep -q "^PermitRootLogin prohibit-password" /etc/ssh/sshd_config; then
-        echo "警告：当前系统配置不允许root使用密码登录SSH。"
+    echo "当前SSH root登录设置:"
+    grep -E "^PermitRootLogin|^PasswordAuthentication" /etc/ssh/sshd_config
+
+    permitRootLogin=$(grep "^PermitRootLogin" /etc/ssh/sshd_config | awk '{print \$2}')
+    passwordAuthentication=$(grep "^PasswordAuthentication" /etc/ssh/sshd_config | awk '{print \$2}')
+
+    echo "解释："
+    if [[ "$permitRootLogin" == "yes" ]]; then
+        echo "- PermitRootLogin yes: 允许root用户通过SSH登录"
+    elif [[ "$permitRootLogin" == "prohibit-password" ]]; then
+        echo "- PermitRootLogin prohibit-password: 禁止root用户使用密码登录SSH，但允许使用其他方式（如密钥）"
+    elif [[ "$permitRootLogin" == "no" ]]; then
+        echo "- PermitRootLogin no: 完全禁止root用户通过SSH登录"
+    fi
+
+    if [[ "$passwordAuthentication" == "yes" ]]; then
+        echo "- PasswordAuthentication yes: 允许使用密码进行SSH认证"
+    elif [[ "$passwordAuthentication" == "no" ]]; then
+        echo "- PasswordAuthentication no: 禁止使用密码进行SSH认证，只能使用密钥等其他方式"
+    fi
+
+    echo "总结："
+    if [[ "$permitRootLogin" == "yes" && "$passwordAuthentication" == "yes" ]]; then
+        echo "当前设置允许使用密码进行root SSH登录。"
+    else
+        echo "警告：当前设置不允许使用密码进行root SSH登录。"
         echo "修改root密码可能对SSH登录没有实际影响。"
         echo "如果你确定要继续，请输入'yes'，否则脚本将退出。"
         read -r response
@@ -47,12 +71,12 @@ fi
 if [ -z "$key" ]; then
     # 生成随机密码
     new_password=$(generate_password)
-    echo "生成了新的root密码: $new_password"
+    echo "生成了新的root密码:$new_password"
 else
     # 使用自定义密码
-    echo -e "\n请输入新的root密码："
+    echo -e "\n请输入新的root密码：（输入不显示内容是正常的）"
     read -s new_password
-    echo "请再次输入新的root密码："
+    echo "请再次输入新的root密码：（输入不显示内容是正常的）"
     read -s new_password_confirm
     
     if [ "$new_password" != "$new_password_confirm" ]; then
@@ -67,18 +91,15 @@ echo "root:$new_password" | chpasswd
 
 # 检查密码是否成功更改
 if [ $? -eq 0 ]; then
-    echo "root密码已经被成功更改为: $new_password"
+    echo "root密码已经被成功更改为:$new_password"
 else
     echo "更改root密码失败，请重试"
     exit 1
 fi
 
-echo "注意：如果系统配置为禁止root密码登录，这个更改可能不会影响SSH登录。"
-echo "请检查 /etc/ssh/sshd_config 文件以确认当前的SSH登录设置。"
+echo "再次提醒：如果系统配置为禁止root密码登录，这个更改可能不会影响SSH登录。"
+echo "您可以自行检查 /etc/ssh/sshd_config 文件以确认当前的SSH登录设置。"
 
-# 显示当前SSH root登录设置
-echo "当前SSH root登录设置:"
-grep -E "^PermitRootLogin|^PasswordAuthentication" /etc/ssh/sshd_config
 
 echo "请确保你已经保存了这个新的root密码:$new_password"
 echo "建议不要直接断开ssh重连，而是新开一个ssh窗口连接尝试"
